d9d277f6fe3cece575da295272a2d9f977aeb563
Amin199473 <amindeveloperlaravel@gmail.com>
2023-07-17 19:05:16 +0330

    tailwind and mix configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import TheExample from './example.vue';
+
+const app = createApp({});
+
+app.component("the-example", TheExample);
+app.mount("#app");
